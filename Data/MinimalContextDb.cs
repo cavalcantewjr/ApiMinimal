9e9d669378ce5ad838498db7d77e61a639ffd48b
Washington Cavalcante <wfcavalcantejr@outlook.com>
2023-08-08 13:41:20 -0300

    Estrutura inicial com modelos, entidades, conexão com banco de dados e migrations

---
+++ Data/MinimalContextDb.cs
@@ -0,0 +1,39 @@
+﻿using ApiMinimal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMinimal.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Ativo)
+                .HasColumnType("bit");
+
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("Fornecedores");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiMinimal.Models
+{
+    public class Fornecedor
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Documento { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
